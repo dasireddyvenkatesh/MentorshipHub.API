0ab9101fb188fad60b8590a7737403d3af769855
Dasireddy Venkatesh <dasireddyvenkatesh@gmail.com>
2026-02-24 15:27:36 +0530

    First API Changes

---
+++ MentorshipHub.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentorshipHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("/login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            LoginResponse loginResponse = new LoginResponse
+            {
+                IsSuccess = true,
+                RequiresMfa = true,
+                Token = "dummy-jwt",
+                Ipaddress = HttpContext.Connection.RemoteIpAddress?.ToString()
+            };
+            return Ok(loginResponse);
+        }
+
+
+        public class LoginRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class LoginResponse
+        {
+            public bool IsSuccess { get; set; }
+            public bool RequiresMfa { get; set; }
+            public string Token { get; set; }
+            public string Ipaddress { get; set; }
+        }
+    }
+}
