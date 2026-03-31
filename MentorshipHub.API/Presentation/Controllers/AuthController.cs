a8fadb4d3edccf8d76a076659d0411d3dc2f468d
Dasireddy Venkatesh <dasireddyvenkatesh@gmail.com>
2026-03-31 11:24:04 +0530

    Bug Fix

--- MentorshipHub.API/Presentation/Controllers/AuthController.cs
+++ MentorshipHub.API/Presentation/Controllers/AuthController.cs
@@ -40,7 +40,7 @@ namespace MentorshipHub.API.Presentation.Controllers
             {
                 HttpOnly = true,
                 Secure = true,
-                SameSite = SameSiteMode.Lax,
+                SameSite = SameSiteMode.None,
                 MaxAge = TimeSpan.FromDays(7),
             });
         }
